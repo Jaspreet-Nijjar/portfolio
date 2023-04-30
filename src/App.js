14cbea3a04a50b425c0f71f3c3bd9666f1481d79
Jaspreet <jaspreetkn92@gmail.com>
2023-04-30 19:14:38 +0100

    added about component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Footer } from './components/Footer';
 import { Contact } from './components/Contact';
+import { About } from './components/About';
 
 function App() {
   return (
     <div className="App">
+      <About />
       <Contact />
       <Footer />
     </div>
---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import '../styles/About.css';
+
+export const About = () => {
+  return (
+    <section class="about-techSkills">
+      <h1>Tech Skills</h1>
+    </section>
+  );
+};
