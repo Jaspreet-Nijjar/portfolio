bfa0134a3eca36ea0505a02a6576e4b2e9402367
Jaspreet <jaspreetkn92@gmail.com>
2023-05-12 08:08:02 +0100

    changed the tech icons background and grid display

--- src/components/About.js
+++ src/components/About.js
@@ -9,25 +9,24 @@ export const About = () => {
         <h1 className="about-large-header">About</h1>
         <p className="about-content">
           I am a Junior Front End Developer based in London. My background is in
-          the sciences which has allowed me to develop my analytical, creative
-          and problem- solving skills which has driven me towards learning Front
-          End Development.
+          the sciences which has allowed me to develop strong analytical,
+          creative and problem-solving skills and driven me towards learning
+          Front End Development.
         </p>
 
         <p className="about-content">
-          Since 2022, I have gained a range of skills including HTML, CSS,
-          JavaScript, React and TypeScript and I love to bring a design to life
-          and enjoy the creative process that adds value and accomplishes the
-          end goal.
+          Since 2022, I have gained a range of skills including
+          <strong> HTML, CSS, JavaScript, React and TypeScript</strong> and I
+          love to bring a design to life and enjoy the creative process that
+          adds value and accomplishes the end goal.
         </p>
       </div>
 
-      {/* <h1>Tech Skills</h1>
       <section class="skills-container">
         {IconsData.map((icon, index) => (
           <TechItem icon={icon} key={index} />
         ))}
-      </section> */}
+      </section>
     </section>
   );
 };
