41ffc0df3168eebb3b95c9e8ecac5aea0312aac5
Jaspreet <jaspreetkn92@gmail.com>
2023-05-12 09:57:30 +0100

    styling the project item

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom';
+import '../styles/ProjectItem.css';
 
 export const ProjectItem = ({ project }) => {
   return (
     <div className="project-item">
-      <img src={project.img} alt="personal project" />
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      {project.tech.map((stack) => (
-        <p>{stack}</p>
-      ))}
+      <img src={project.img} alt="personal project" className="project-image" />
+      <h3 className="project-title">{project.title}</h3>
+      <p className="project-description">{project.description}</p>
+
+      <div className="project-stack">
+        {project.tech.map((stack) => (
+          <p className="project-stack-item">{stack}</p>
+        ))}
+      </div>
+
       <Link to={project.code}>View Code</Link>
       <Link to={project.preview}>Live Preview</Link>
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ import { ProjectItem } from './ProjectItem';
 export const Projects = () => {
   return (
     <section id="projects">
-      <h1 className="large-header">Projects</h1>
+      <h1 className="projects-large-header">Projects</h1>
 
       <section class="projects-container">
         {ProjectsData.map((project, index) => (
