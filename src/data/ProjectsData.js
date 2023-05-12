4566bf10633933d57737d51ddbdca23f2a91ba36
Jaspreet <jaspreetkn92@gmail.com>
2023-05-12 12:40:31 +0100

    added shopping project

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,6 +1,7 @@
 import saintk9 from '../assets/project-images/saintk9.png';
 import currency from '../assets/project-images/currency.png';
 import password from '../assets/project-images/password.png';
+import shopping from '../assets/project-images/shopping.png';
 
 export const ProjectsData = [
   {
@@ -11,6 +12,16 @@ export const ProjectsData = [
     tech: ['React', 'JavaScript', 'CSS'],
     code: 'https://github.com/Jaspreet-Nijjar/saint-k9-training',
     preview: 'https://jn-saint-k9-training.netlify.app/',
+  },
+
+  {
+    img: shopping,
+    title: 'Ecommerce Product Store',
+    description:
+      'A simple e-commerce product store using the context hook to add functionalities. ',
+    tech: ['React', 'JavaScript', 'CSS'],
+    code: 'https://github.com/Jaspreet-Nijjar/ecommerce-store',
+    preview: 'https://jn-ecommerce-project.netlify.app/',
   },
 
   {
